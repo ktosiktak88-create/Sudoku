a8a0f39bdbbc7661ff48413bc41b3b77b77157a6
ktosiktak88-create <ktosiktak88@gmail.com>
2026-09-05 03:53:31 +0200

    Use stable Android 16 SDK for APK build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.twojastudio.sudokucodziennie"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.twojastudio.sudokucodziennie"
